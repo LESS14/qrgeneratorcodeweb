ea406121bc7711e12825ff19429253c286da30d3
LESS14 <100519912+LESS14@users.noreply.github.com>
2023-11-09 05:26:53 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
